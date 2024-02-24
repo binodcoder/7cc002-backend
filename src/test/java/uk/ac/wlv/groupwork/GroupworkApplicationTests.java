c05aa9eb3c7141413194231785f749e2ed9ad829
Sahan Perera <sahanyuganthamail@gmail.com>
2024-02-24 22:10:42 +0000

    initial commit

---
+++ src/test/java/uk/ac/wlv/groupwork/GroupworkApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.wlv.groupwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
