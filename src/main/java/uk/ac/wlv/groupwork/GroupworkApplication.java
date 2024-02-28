913fc257dba09699039fe44ba0e29d424ff41804
Sahan Perera <sahanyuganthamail@gmail.com>
2024-02-28 22:58:39 +0000

    mvn plugin changes 1

--- src/main/java/uk/ac/wlv/groupwork/GroupworkApplication.java
+++ src/main/java/uk/ac/wlv/groupwork/GroupworkApplication.java
@@ -2,10 +2,19 @@ package uk.ac.wlv.groupwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class GroupworkApplication {
 
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello";
+    }
     public static void main(String[] args) {
         SpringApplication.run(GroupworkApplication.class, args);
     }
