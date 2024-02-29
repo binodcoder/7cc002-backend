e8b62577165e55b94ddcbb1dc391b7afbf274d34
Sahan Perera <sahanyuganthamail@gmail.com>
2024-02-29 00:39:05 +0000

    pom updated 5

--- src/main/java/uk/ac/wlv/groupwork/GroupworkApplication.java
+++ src/main/java/uk/ac/wlv/groupwork/GroupworkApplication.java
@@ -2,19 +2,20 @@ package uk.ac.wlv.groupwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
-public class GroupworkApplication {
+public class GroupworkApplication extends SpringBootServletInitializer {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home(){
-        return "Hello";
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GroupworkApplication.class);
     }
+
     public static void main(String[] args) {
         SpringApplication.run(GroupworkApplication.class, args);
     }
