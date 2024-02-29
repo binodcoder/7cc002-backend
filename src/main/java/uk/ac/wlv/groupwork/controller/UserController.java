9c3a637e43c343e64d52878584fedc322552cd8c
Sahan Perera <sahanyuganthamail@gmail.com>
2024-02-29 01:44:08 +0000

    pom updated user controller 2

--- src/main/java/uk/ac/wlv/groupwork/controller/UserController.java
+++ src/main/java/uk/ac/wlv/groupwork/controller/UserController.java
@@ -3,6 +3,7 @@ package uk.ac.wlv.groupwork.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import uk.ac.wlv.groupwork.model.User;
 import uk.ac.wlv.groupwork.service.UserService;
@@ -19,6 +20,7 @@ public class UserController {
     }
 
     @GetMapping
+    @ResponseBody
     public Object get(){
         return userService.getAll();
     }
