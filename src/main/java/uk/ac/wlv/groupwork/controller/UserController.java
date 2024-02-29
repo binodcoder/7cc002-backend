b574b9dabf58d8d2053e0b855565b3f0a37e4ccc
Sahan Perera <sahanyuganthamail@gmail.com>
2024-02-29 01:55:06 +0000

    pom updated user controller 5

--- src/main/java/uk/ac/wlv/groupwork/controller/UserController.java
+++ src/main/java/uk/ac/wlv/groupwork/controller/UserController.java
@@ -1,7 +1,6 @@
 package uk.ac.wlv.groupwork.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.ac.wlv.groupwork.model.User;
 import uk.ac.wlv.groupwork.service.UserService;
 
+import java.util.ArrayList;
+import java.util.List;
 
-@Controller
+
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
@@ -21,9 +23,22 @@ public class UserController {
     }
 
     @GetMapping
-    @ResponseBody
     public Object get(){
-        return userService.getAll();
+        return fakeDbCall();
     }
+
+    private List<User> fakeDbCall(){
+
+        List<User> userList = new ArrayList<>();
+
+        userList.add(new User(1,"sahan@bcas.lk", "sahan", "123", "USER"));
+        userList.add(new User(2,"sam@gmail.com", "Sammy", "455", "ADMIN"));
+        userList.add(new User(3,"anne@yahoo.com", "Anne", "789", "STAFF"));
+        userList.add(new User(4,"ron@gmail.com", "Ronald", "455", "STAFF"));
+        userList.add(new User(5,"will@yahoo.com", "William", "123", "USER"));
+
+        return userList;
+    }
+
 }
 
