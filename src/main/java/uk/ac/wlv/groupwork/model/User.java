837e3284a9ce05edf48ec37030925ea1b8f01148
Sahan Perera <sahanyuganthamail@gmail.com>
2024-03-05 21:20:04 +0000

    user model updated

--- src/main/java/uk/ac/wlv/groupwork/model/User.java
+++ src/main/java/uk/ac/wlv/groupwork/model/User.java
@@ -7,20 +7,21 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String name;
+
     private String email;
     private String password;
-    private String name;
     private String gender;
     private int age;
 
     public User() {
     }
 
-    public User(int id, String email, String password, String name, String gender, int age) {
+    public User(int id, String name, String email, String password, String gender, int age) {
         this.id = id;
+        this.name = name;
         this.email = email;
         this.password = password;
-        this.name = name;
         this.gender = gender;
         this.age = age;
     }
@@ -31,6 +32,14 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -47,14 +56,6 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getGender() {
